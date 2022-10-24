565749637095a978daa9d2507a26ec9b496b8f92
Paul Thomas <paulthomas2107@gmail.com>
2022-10-24 11:20:31 +0100

    More....

--- svelte-feedback-app/src/App.svelte
+++ svelte-feedback-app/src/App.svelte
@@ -1,75 +1,36 @@
 <script>
-  import { toggle_class } from 'svelte/internal';
 
-  let firstName = 'Paul A';
-  let lastName = 'Thomas';
-  let color = 'blue';
-  let showText = false;
-  let users = [
+	
+import FeedbackList from './components/FeedbackList.svelte';
+
+  let feedback = [
 	{
-		id: "1",
-		name: "Caitlin Thomas"
+		id: 1,
+		rating: 10,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio hic nisi, facere ipsa optio debitis repellat iusto amet animi id laborum. Accusantium quaerat saepe veritatis, sunt iste ea magnam numquam.'
 	},
 	{
-		id: "2",
-		name: "Rory Thomas"
+		id: 2,
+		rating: 9,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio hic nisi, facere ipsa optio debitis repellat iusto amet animi id laborum. Accusantium quaerat saepe veritatis, sunt iste ea magnam numquam.'
 	},
 	{
-		id: "3",
-		name: "Paul Thomas"
-	},
+		id: 3,
+		rating: 8,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio hic nisi, facere ipsa optio debitis repellat iusto amet animi id laborum. Accusantium quaerat saepe veritatis, sunt iste ea magnam numquam.'
+	}
   ]
 
-  $: name = firstName + ' ' + lastName;
 
-  const toggle = () => {
-    color = color === 'blue' ? 'red' : 'blue';
-    showText = !showText;
-	users = [...users, {id: "4", name: "Jim Cad"}]
-  };
+  
 </script>
 
-<main>
-  <h1 style="color: {color};">Hello {name} !</h1>
-  {#if showText}
-    <p>
-      Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-      how to build Svelte apps.
-    </p>
-  {:else}
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum maxime eius
-      excepturi ullam in porro doloremque distinctio quaerat fuga perferendis,
-      minima assumenda modi enim, incidunt, alias natus numquam consequatur
-      aperiam?
-    </p>
-  {/if}
-  <button on:click={toggle}>Click</button>
-
-  {#each users as user (user.id) }
-	<h3>{user.id}: {user.name}</h3>
-  {/each}
+<main class="container">
+  <FeedbackList feedback={feedback} />
 	
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
+  
+  
 </style>
