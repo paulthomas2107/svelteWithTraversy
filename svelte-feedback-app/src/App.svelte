7914a5d9fbdc32b926ac66e41bd4da08e28486c4
Paul Thomas <paulthomas2107@gmail.com>
2022-10-24 10:35:45 +0100

    More.....

--- svelte-feedback-app/src/App.svelte
+++ svelte-feedback-app/src/App.svelte
@@ -1,52 +1,74 @@
 <script>
-  import { toggle_class } from "svelte/internal";
+  import { toggle_class } from 'svelte/internal';
 
+  let firstName = 'Paul A';
+  let lastName = 'Thomas';
+  let color = 'blue';
+  let showText = false;
+  let users = [
+	{
+		id: "1",
+		name: "Caitlin Thomas"
+	},
+	{
+		id: "2",
+		name: "Rory Thomas"
+	},
+	{
+		id: "3",
+		name: "Paul Thomas"
+	},
+  ]
 
-	let firstName = "Paul A";
-	let lastName = "Thomas";
-	let color = 'blue';
-	let showText = false;
+  $: name = firstName + ' ' + lastName;
 
-	$: name = firstName + " " + lastName;
-
-	const toggle = () => {
-		color = color === 'blue' ? 'red' : 'blue';
-		showText = !showText;
-	}
-
-	
+  const toggle = () => {
+    color = color === 'blue' ? 'red' : 'blue';
+    showText = !showText;
+  };
 </script>
 
 <main>
-	<h1 style="color: {color};">Hello {name}  !</h1>
-	{#if showText}
-		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	{:else}
-	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum maxime eius excepturi 
-		ullam in porro doloremque distinctio quaerat fuga perferendis, minima assumenda modi enim,
-		incidunt, alias natus numquam consequatur aperiam?</p>
-	{/if}
-	<button on:click={toggle}>Click</button>
+  <h1 style="color: {color};">Hello {name} !</h1>
+  {#if showText}
+    <p>
+      Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+      how to build Svelte apps.
+    </p>
+  {:else}
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum maxime eius
+      excepturi ullam in porro doloremque distinctio quaerat fuga perferendis,
+      minima assumenda modi enim, incidunt, alias natus numquam consequatur
+      aperiam?
+    </p>
+  {/if}
+  <button on:click={toggle}>Click</button>
+
+  {#each users as user (user.id) }
+	<h3>{user.id}: {user.name}</h3>
+  {/each}
+	
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
