b99f011fb74d5d9798cdd8c535177e05ea04db13
Paul Thomas <paulthomas2107@gmail.com>
2022-10-24 12:01:25 +0100

    Added form

--- svelte-feedback-app/src/App.svelte
+++ svelte-feedback-app/src/App.svelte
@@ -1,39 +1,39 @@
 <script>
-
-	
-import FeedbackList from './components/FeedbackList.svelte';
+  import FeedbackList from './components/FeedbackList.svelte';
+  import FeedbackStats from './components/FeedbackStats.svelte';
 
   let feedback = [
-	{
-		id: 1,
-		rating: 10,
-		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio hic nisi, facere ipsa optio debitis repellat iusto amet animi id laborum. Accusantium quaerat saepe veritatis, sunt iste ea magnam numquam.'
-	},
-	{
-		id: 2,
-		rating: 9,
-		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio hic nisi, facere ipsa optio debitis repellat iusto amet animi id laborum. Accusantium quaerat saepe veritatis, sunt iste ea magnam numquam.'
-	},
-	{
-		id: 3,
-		rating: 8,
-		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio hic nisi, facere ipsa optio debitis repellat iusto amet animi id laborum. Accusantium quaerat saepe veritatis, sunt iste ea magnam numquam.'
-	}
-  ]
+    {
+      id: 1,
+      rating: 10,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio hic nisi, facere ipsa optio debitis repellat iusto amet animi id laborum. Accusantium quaerat saepe veritatis, sunt iste ea magnam numquam.',
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio hic nisi, facere ipsa optio debitis repellat iusto amet animi id laborum. Accusantium quaerat saepe veritatis, sunt iste ea magnam numquam.',
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio hic nisi, facere ipsa optio debitis repellat iusto amet animi id laborum. Accusantium quaerat saepe veritatis, sunt iste ea magnam numquam.',
+    },
+  ];
 
+  $: count = feedback.length;
+  $: average =
+    feedback.reduce((a, { rating }) => a + rating, 0) / feedback.length;
 
   const deleteFeedback = (e) => {
-	const itemId = e.detail
-	feedback = feedback.filter((item) => item.id != itemId)
-  }
+    const itemId = e.detail;
+    feedback = feedback.filter((item) => item.id != itemId);
+  };
 </script>
 
 <main class="container">
-  <FeedbackList feedback={feedback} on:delete-feedback={deleteFeedback} />
-	
+  <FeedbackStats {count} {average} />
+  <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
 
 <style>
-  
-  
 </style>
